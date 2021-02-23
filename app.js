8f10852b554d869065e1ae0a660bba1d5d3ed1fd
Dragan Milovac <d.milovac@rs.emakina.com>
2021-02-23 10:56:05 +0100

    Add MERN database and CORS

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With,Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+});
+
 app.use("/api/places", routerPlaces);
 app.use("/api/users", usersRoutes);
 
@@ -28,7 +37,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://dragan:Pass4Emakina@cluster0.riugc.mongodb.net/places?retryWrites=true&w=majority",
+    "mongodb+srv://dragan:Pass4Emakina@cluster0.riugc.mongodb.net/mern?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
