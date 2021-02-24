5a73d891a1703f930713e2c286a2bf42b94c0dfa
Dragan Milovac <d.milovac@rs.emakina.com>
2021-02-24 15:21:55 +0100

    Finishing upload user images

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -40,8 +40,7 @@ const signup = async (req, res, next) => {
   const createdUser = new User({
     name,
     email,
-    image:
-      "https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png",
+    image: req.file.path,
     password,
     places: [],
   });
