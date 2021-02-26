7e22ecb3fb6374dbd303585503de5688d25c0647
Dragan Milovac <d.milovac@rs.emakina.com>
2021-02-26 14:06:24 +0100

    Add env variables in backend

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://dragan:VntqgLgc0FT8xwnK@cluster0.riugc.mongodb.net/mern?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.riugc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => {
--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -71,7 +71,7 @@ const signup = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: createdUser.id, email: createdUser.email },
-      "supersecret_dont_share",
+      process.env.JWT_TOKEN,
       { expiresIn: "1h" }
     );
   } catch (err) {
@@ -133,7 +133,7 @@ const login = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
-      "supersecret_dont_share",
+      process.env.JWT_TOKEN,
       { expiresIn: "1h" }
     );
   } catch (err) {
